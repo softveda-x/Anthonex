946e094950765b619c7ad72a5f39c085e7f26bdf
softveda-x <merajalam8294@gmail.>
2026-03-04 00:33:58 +0530

    Feature added

---
+++ javascript/F_script.js
@@ -0,0 +1,8 @@
+// FAQ Toggle
+document.querySelectorAll(".faq-item").forEach(item => {
+  item.addEventListener("click", () => {
+    const answer = item.querySelector(".faq-answer");
+    answer.style.display =
+      answer.style.display === "block" ? "none" : "block";
+  });
+});
